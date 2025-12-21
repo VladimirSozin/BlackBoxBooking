17aacf64d9cd0ce5e398088b1f3db9238a1e1967
loginov.dn <loginovdn.it@relef.ru>
2025-12-21 12:02:52 +0300

    fix

---
+++ src/Vacation.Api/DependencyInjection.cs
@@ -0,0 +1,10 @@
+namespace Vacation.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        services.AddControllers();
+        return services;
+    }
+}
